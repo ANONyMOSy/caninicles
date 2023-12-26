6cbe9df573554cf169015a6230f0f10cca369225
ANONyMOSy <jameskaddissi@gmail.com>
2023-12-26 04:42:55 -0500

    Finished player and camer movement. Started player model

--- Caninicles/Assets/PlayerController.cs
+++ Caninicles/Assets/PlayerController.cs
@@ -22,5 +22,50 @@ public class PlayerController : MonoBehaviour
 
     void Awake() {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    void AssignInputs() {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    void ClickToMove() {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers)){
+            agent.destination = hit.point;
+            if(clickEffect != null) {
+                Instantiate(clickEffect, hit.point += new Vector3(0, 0.1f, 0), clickEffect.transform.rotation);
+            }
+        }
+    }
+
+    void OnEnable() {
+        input.Enable();
+    }
+
+    void OnDisable() {
+        input.Disable();
+    }
+
+    void Update() {
+        FaceTarget();
+        SetAnimations();
+    }
+
+    void FaceTarget() {
+        Vector3 direction = (agent.destination - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+    }
+
+    void SetAnimations() {
+        if(agent.velocity == Vector3.zero) {
+            animator.Play(IDLE);
+        } else {
+            animator.Play(WALK);
+        }
     }
 }
